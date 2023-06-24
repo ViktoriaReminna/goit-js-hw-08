2b76714e87bf7e53d11dcd1b523d557f56759bc8
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-06-24 22:09:46 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Vimeo from '@vimeo/player';
+// console.log(Vimeo);
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  const currentTime = JSON.stringify(data);
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}
+
+const restoreCurrentTime = () => {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  if (currentTime) {
+    const timeJSON = JSON.parse(currentTime);
+    const { seconds } = timeJSON;
+    return seconds;
+  }
+};
+
+player
+  .setCurrentTime(restoreCurrentTime())
+  .then(function (seconds) {})
+
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const inputRef = document.querySelector('.feedback-form input');
+const formData = {};
 
+const STORAGE_KEY = 'feedback-form-state';
+
+// const { email, message } = formRef.elements.value;
+// console.dir(formRef);
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+const onInput = event => {
+  formData[event.target.name] = event.target.value;
+  //   console.log(value);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onInput, 500));
+
+const populateTextarea = () => {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    const state = JSON.parse(savedMessage);
+    inputRef.value = state.email;
+    textareaRef.value = state.message;
+  }
+};
+populateTextarea();
